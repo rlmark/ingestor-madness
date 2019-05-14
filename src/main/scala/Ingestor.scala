c7a9a36d9eabf6c3194b2f7c399642587babac68
rebeccamark <rebecca.mark@47deg.com>
2019-05-14 09:18:23 -0700

    Generic return type in Ingestor

--- src/main/scala/Ingestor.scala
+++ src/main/scala/Ingestor.scala
@@ -2,16 +2,15 @@ import cats.{Applicative, Traverse}
 
 // Here we have 2 implementations for the Ingestor trait.
 // They read from different sources create a subtype of a DataBag
-sealed trait Ingestor[F[_], O] {
-//  def read(): F[Vector[O]]
-    def read(): F[Vector[DataBag]] // Attempt 2
+sealed trait Ingestor[F[_],O] {
+    def read(): F[Vector[O]]
 }
 
-class StringIngestorImpl[F[_]: Applicative ] extends Ingestor[F, Vector[DataBag]] {
+class StringIngestorImpl[F[_]: Applicative ] extends Ingestor[F, StringBag] {
   // Imagine that this gets a bunch of data from some place
-  override def read(): F[Vector[DataBag]] = Applicative[F].pure(Vector(StringBag("HI")))
+  override def read(): F[Vector[StringBag]] = Applicative[F].pure(Vector(StringBag("HI")))
 }
 
-class NumberIngestorImpl[F[_]: Applicative ] extends Ingestor[F, Vector[DataBag]] {
-  override def read(): F[ Vector[DataBag]] = Applicative[F].pure(Vector(NumberBag(9)))
+class NumberIngestorImpl[F[_]: Applicative ] extends Ingestor[F, NumberBag] {
+  override def read(): F[ Vector[NumberBag]] = Applicative[F].pure(Vector(NumberBag(9)))
 }
